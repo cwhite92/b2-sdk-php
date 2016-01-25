50f4b0f43c2b0f7edc24218c9f67a99c47fa120e
Chris White <chris@cwhite.me>
2016-01-25 14:41:44 +0000

    lets get rid of my account credentials shall we

--- run.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require ('vendor/autoload.php');
-
-$client = new \ChrisWhite\B2\Client('cb561ac2f6eb', '001415c09c2139b77637a05b155baeef43c0b225cd');
-
-$bucket = $client->createBucket('test-bucsdfsdfket-fh7f', \ChrisWhite\B2\Bucket::TYPE_PUBLIC);
-
-$client->updateBucket($bucket->getId(), \ChrisWhite\B2\Bucket::TYPE_PRIVATE);
