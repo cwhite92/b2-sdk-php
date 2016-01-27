ab2e0b400cc3d2917617165807125b192213a8b2
Chris White <chris@cwhite.me>
2016-01-27 21:11:10 +0000

    Provide a nice user agent string for HTTP requests

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -11,5 +11,16 @@ use GuzzleHttp as GuzzleClient;
  */
 class Client extends GuzzleClient
 {
-
+    /**
+     * Provide a responsible and helpful user agent when making HTTP requests.
+     *
+     * @return string
+     */
+    public function getDefaultUserAgent()
+    {
+        return 'b2-sdk-php'.
+            ' Guzzle/'.curl_version()['version'].
+            ' cURL'.
+            ' PHP/'.PHP_VERSION;
+    }
 }
